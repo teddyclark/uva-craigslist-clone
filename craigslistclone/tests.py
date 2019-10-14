83372bfbb933f99a203af05ac4fbc70909d423d6
payalasri <43188166+payalasri@users.noreply.github.com>
2019-10-13 22:50:53 -0400

    Update tests.py
    second test for login page

--- craigslistclone/tests.py
+++ craigslistclone/tests.py
@@ -4,9 +4,12 @@ from django.test import SimpleTestCase
 
 # Create your tests here.
 class LoginTests(SimpleTestCase):
-    #tests to see if it gets to the login page
+    #if the url works
     def test_login_to_home(self): 
         response = self.client.get('/')
         self.assertEquals(response.status_code, 200)
-    
 
+    #if the url doesn't work
+    def test_login_to_home_broke(self):
+        response = self.client.get('/')
+        self.assertNotContains(response, 'It did not work')
