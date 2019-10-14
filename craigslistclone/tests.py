2077ec67348cc92ccf70c9fa743cf9d2abd627f0
payalasri <43188166+payalasri@users.noreply.github.com>
2019-10-13 22:38:57 -0400

    Update tests.py
    first test

--- craigslistclone/tests.py
+++ craigslistclone/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.http import HttpRequest 
+from django.test import SimpleTestCase
 
 # Create your tests here.
+class LoginTests(SimpleTestCase):
+    #tests to see if it gets to the login page
+    def test_login_to_home(self): 
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+    
+
