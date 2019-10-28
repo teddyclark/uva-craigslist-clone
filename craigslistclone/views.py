1ec8aaba15ce56e35bafd5535598690c2776f06a
priprichung <pjc8mp@virginia.edu>
2019-10-28 02:00:31 -0400

    forgot to add views.py after the merge of ui-template and post-backup

--- craigslistclone/views.py
+++ craigslistclone/views.py
@@ -1,13 +1,12 @@
-# from django.urls import reverse_lazy
+from django.urls import reverse_lazy
 # from .utils import is_login
-# from .forms import RegisterForm
+from .forms import ListingForm
+from django.views.generic import CreateView
+from django.http import HttpResponse, HttpRequest, HttpResponseBadRequest, HttpResponseForbidden
 from django.shortcuts import render, redirect
 from django.urls import reverse
 # from django.contrib import messages
-from.models import User
-
-from django.http import HttpResponse, HttpRequest, HttpResponseBadRequest, HttpResponseForbidden
-
+from.models import User, Listing
 
 def home(request):
     if request.user.is_authenticated:
@@ -26,6 +25,20 @@ def welcome(request):
         return render(request, 'welcome.html')
     else:
         return render(request, 'home.html')
+
+
+class CreateListing(CreateView):
+    template_name = 'createListing.html'
+    form_class = ListingForm
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        print("form valid")
+        # need to define Key to User 
+        #creator = User.objects.get(id=self.request.session['user'])
+        # for now no user
+        #form.instance.creator = creator
+        return super(CreateListing, self).form_valid(form)
 
 
 
