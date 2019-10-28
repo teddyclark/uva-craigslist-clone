4faa5c1209665db4e7165ebbd73bf6e70c0d54f9
priprichung <pjc8mp@virginia.edu>
2019-10-28 01:54:38 -0400

    merging ui-template and post-backup together

--- craigslistclone/admin.py
+++ craigslistclone/admin.py
@@ -1,5 +1,5 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import User, Listing
 
-# from .models import User
-
-# admin.site.register(User)
+admin.site.register(User)
+admin.site.register(Listing)
