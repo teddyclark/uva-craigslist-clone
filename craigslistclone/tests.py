1276c0b9104fe557ab06ad2361434b7b87f4db0c
payalasri <43188166+payalasri@users.noreply.github.com>
2019-11-03 15:30:54 -0500

    Adding test cases for Models

--- craigslistclone/tests.py
+++ craigslistclone/tests.py
@@ -1,9 +1,12 @@
-from django.test import TestCase
-
 import unittest
 
+from django.test import TestCase, Client
+from django.urls import reverse, resolve
+from craigslistclone.models import User
+from craigslistclone.views import home
 
 class TestStringMethods(unittest.TestCase):
+    # temporary tests
     def test_1(self):
         a = 1
         self.assertEqual(1, a)
@@ -11,3 +14,21 @@ class TestStringMethods(unittest.TestCase):
         b = 4
         a = 1
         self.assertFalse(a == b)
+
+class TestUrl(unittest.TestCase):
+    # tests root url resolves to home
+    def test_root_url_resolves_to_home_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home)
+class TestUserModel(unittest.TestCase):
+    def test_usernameTest(self):
+        user = User(username = 'tester')
+        self.assertEqual(user.username, "tester")
+    def test_first_name(self):
+        user = User(firstName = "John")
+        self.assertEqual(user.firstName, "John")
+    # exception test
+    def test_last_name(self):
+        user = User(lastName = "Doe")
+        self.assertFalse(user.lastName == "Day")    
+    
