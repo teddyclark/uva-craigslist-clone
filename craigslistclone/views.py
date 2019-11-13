577c5fd3e38fc1ba0a13c92257fc26b690ed2ac3
Bryce <bc6cd@virginia.edu>
2019-11-12 23:50:35 -0500

    added connection between users and posts - bug in displaying photos

--- craigslistclone/views.py
+++ craigslistclone/views.py
@@ -11,6 +11,7 @@ from.models import User, Listing, GoogleUserList
 from django.views import generic
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 
 def home(request):
     if request.user.is_authenticated:
@@ -34,17 +35,42 @@ def logout(request):
 #     else:
 #         return render(request, 'login.html')
 
-class CreateListing(LoginRequiredMixin, CreateView):
-    login_url = '/'
-    template_name = 'createListing.html'
-    form_class = ListingForm
+# class CreateListing(LoginRequiredMixin, CreateView):
+#     login_url = '/'
+#     template_name = 'createListing.html'
+#     form_class = ListingForm
 
-    # form_class = ListingForm
-    success_url = reverse_lazy('home')
+#     # form_class = ListingForm
+#     success_url = reverse_lazy('home')
 
-    def form_valid(self, form):
-        print("form valid")
-        return super(CreateListing, self).form_valid(form)
+#     def form_valid(self, form):
+#         print("form valid")
+#         return super(CreateListing, self).form_valid(form)
+
+
+
+
+def createListing(request, template_name="createListing.html"):
+    if request.user.is_authenticated:
+        user = User.objects.get(username=request.user.username)
+        # staffprofile.user = user
+        print(" FUCKKKKKKKK", user)
+        if request.method == 'POST':
+            form = ListingForm(request.POST)
+            print("USERNAME: ", request.user.username)
+            if form.is_valid():
+                instance = form.save(commit=False)
+                # Listing.associated_username = request.user.username
+                instance.associated_username = user
+                instance.save()
+                return redirect('home')
+        else:
+            form = ListingForm()
+        return render(request, template_name, {'form': form})
+    else:
+        return render(request, 'login.html')
+
+
 
 """ This function just spits out all of the posts that have been made at the moment """
 class ListingView(generic.ListView):
