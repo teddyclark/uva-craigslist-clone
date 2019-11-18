f6d3ec7bf7b7f214a262710a512cad224f3b8d69
payalasri <43188166+payalasri@users.noreply.github.com>
2019-11-18 11:19:54 -0500

    Adding test cases

--- craigslistclone/tests.py
+++ craigslistclone/tests.py
@@ -2,9 +2,10 @@ import unittest
 
 from django.test import TestCase, Client
 from django.urls import reverse, resolve
-from craigslistclone.models import Listing
+from craigslistclone.models import User, Listing
 from craigslistclone.views import home
-from users.models import CustomUser
+from django.utils import timezone
+from django.db import models
 
 class TestStringMethods(unittest.TestCase):
     # temporary tests
@@ -16,23 +17,26 @@ class TestStringMethods(unittest.TestCase):
         a = 1
         self.assertFalse(a == b)
 
+# tests root url resolves to home
 class TestUrl(unittest.TestCase):
-    # tests root url resolves to home
     def test_root_url_resolves_to_home_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home)
+
+# tests user model to make sure fields are correct 
 class TestUserModel(unittest.TestCase):
     def test_usernameTest(self):
-        user = CustomUser(username = 'tester')
+        user = User(username = 'tester')
         self.assertEqual(user.username, "tester")
     def test_first_name(self):
-        user = CustomUser(first_name = "John")
-        self.assertEqual(user.first_name, "John")
+        user = User(firstName = "John")
+        self.assertEqual(user.firstName, "John")
     # exception test
     def test_last_name(self):
-        user = CustomUser(last_name = "Doe")
-        self.assertFalse(user.last_name == "Day")
+        user = User(lastName = "Doe")
+        self.assertFalse(user.lastName == "Day")  
 
+# tests access when not logged in
 class ViewsTestNoUser(TestCase):
     def setUp(self):
         self.client = Client()
@@ -46,24 +50,24 @@ class ViewsTestNoUser(TestCase):
     def testProfilePage(self):
         response = self.client.get("profile/")
         self.assertEqual(response.status_code, 404)
-    #def testCreatePostPage(self):                  #this is a bug
-    #    response = self.client.get("/createListing/")
-    #    self.assertEqual(response.status_code, 404)
-    #def testListingsPage(self):
-    #    response = self.client.get("/listings/")
-    #    self.assertEqual(response.status_code, 404)
-'''
+    def testCreatePostPage(self):                  #this is a bug
+       response = self.client.get("/createListing/")
+       self.assertEqual(response.status_code, 302)
+
+
 class ViewsTest(TestCase):
     def setUp(self):
-        #user = GoogleUserList(registered_user = "john")
-        self.client = Client()
-        #self.client.force_login(user)
-        self.client.force_login(User.objects.get_or_create(username='testuser')[0])
-    #def testLogInPage(self):
-    #    response = self.client.get("/")
-    #    self.assertEqual(response.status_code, 200)
-'''
+        self.user = User.objects.create(username='testuser')
+        login = self.client.login(username='testuser')
+        # user = GoogleUserList(registered_user = "john")
+        # self.client = Client()
+        # # self.client.force_login(user)
+        # self.client.force_login(User.objects.get_or_create(username='testuser')[0])
+    def testLogInPage(self):
+       response = self.client.get("/")
+       self.assertEqual(response.status_code, 200)
 
+# tests features about the listing
 class TestListing(unittest.TestCase):
     def test_listingname(self):
         listing = Listing(name = 'testname')
@@ -72,5 +76,13 @@ class TestListing(unittest.TestCase):
         listing = Listing(name = 'testname', price = 51.12)
         self.assertEqual(listing.price, 51.12)
 
-
-    
+class TestListingTime(unittest.TestCase):
+    def test_upload_time(self):
+        listing = Listing(created_at = timezone.now())
+        self.assertIs(listing.created_at, listing.created_at)
+    # def test_listingname(self):
+    #     listing = Listing(created_at = 'testname')
+    #     self.assertEqual(listing.name, "testname")
+    # def test_price(self):
+    #     listing = Listing(name = 'testname', price = 51.12)
+    #     self.assertEqual(listing.price, 51.12)
